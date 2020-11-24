b1c67657763a02f1cc47acdd9bad1180af6b7d28
pahry182 <pahry182@gmail.com>
2020-11-24 19:16:23 +0700

    changing all vertex2i to vertex2f

--- Project/main.cpp
+++ Project/main.cpp
@@ -13,10 +13,10 @@ void mainBackground()
     glColor3ub(16,42,52);
     glBegin(GL_QUADS);
 
-        glVertex2i(0,0);
-        glVertex2i(0,640);
-        glVertex2i(360,640);
-        glVertex2i(360,0);
+        glVertex2f(0,0);
+        glVertex2f(0,640);
+        glVertex2f(360,640);
+        glVertex2f(360,0);
 
     glEnd();
     glFlush();
@@ -26,20 +26,20 @@ void animasiApi()
     glBegin(GL_QUADS);
     glColor3ub(240,141,3);
 
-        glVertex2i(165,315);
-        glVertex2i(170,315);
-        glVertex2i(170,312);
-        glVertex2i(165,312);
+        glVertex2f(165,315);
+        glVertex2f(170,315);
+        glVertex2f(170,312);
+        glVertex2f(165,312);
 
-        glVertex2i(166,312);
-        glVertex2i(169,312);
-        glVertex2i(169,308);
-        glVertex2i(166,308);
+        glVertex2f(166,312);
+        glVertex2f(169,312);
+        glVertex2f(169,308);
+        glVertex2f(166,308);
 
-        glVertex2i(167,308);
-        glVertex2i(168,308);
-        glVertex2i(168,305);
-        glVertex2i(167,305);
+        glVertex2f(167,308);
+        glVertex2f(168,308);
+        glVertex2f(168,305);
+        glVertex2f(167,305);
 
     glEnd();
     glFlush();
@@ -51,173 +51,173 @@ void mainCharacter()
     //CHARA_BADAN
     glColor3ub(202,11,1);
 
-        glVertex2i(173,341);
-        glVertex2i(190,341);
-        glVertex2i(190,339);
-        glVertex2i(173,339);
+        glVertex2f(173,341);
+        glVertex2f(190,341);
+        glVertex2f(190,339);
+        glVertex2f(173,339);
 
-        glVertex2i(172,339);
-        glVertex2i(191,339);
-        glVertex2i(191,338);
-        glVertex2i(172,338);
+        glVertex2f(172,339);
+        glVertex2f(191,339);
+        glVertex2f(191,338);
+        glVertex2f(172,338);
 
-        glVertex2i(172,338);
-        glVertex2i(192,338);
-        glVertex2i(192,334);
-        glVertex2i(172,334);
+        glVertex2f(172,338);
+        glVertex2f(192,338);
+        glVertex2f(192,334);
+        glVertex2f(172,334);
 
-        glVertex2i(174,334);
-        glVertex2i(192,334);
-        glVertex2i(192,324);
-        glVertex2i(174,324);
+        glVertex2f(174,334);
+        glVertex2f(192,334);
+        glVertex2f(192,324);
+        glVertex2f(174,324);
 
-        glVertex2i(176,324);
-        glVertex2i(192,324);
-        glVertex2i(192,320);
-        glVertex2i(176,319);
+        glVertex2f(176,324);
+        glVertex2f(192,324);
+        glVertex2f(192,320);
+        glVertex2f(176,319);
 
-        glVertex2i(176,319);
-        glVertex2i(192,320);
-        glVertex2i(196,320);
-        glVertex2i(196,319);
+        glVertex2f(176,319);
+        glVertex2f(192,320);
+        glVertex2f(196,320);
+        glVertex2f(196,319);
 
-        glVertex2i(186,319);
-        glVertex2i(197,319);
-        glVertex2i(188,317);
-        glVertex2i(186,317);
+        glVertex2f(186,319);
+        glVertex2f(197,319);
+        glVertex2f(188,317);
+        glVertex2f(186,317);
 
-        glVertex2i(188,317);
-        glVertex2i(196,319);
-        glVertex2i(197,319);
-        glVertex2i(197,317);
+        glVertex2f(188,317);
+        glVertex2f(196,319);
+        glVertex2f(197,319);
+        glVertex2f(197,317);
 
-        glVertex2i(188,317);
-        glVertex2i(197,317);
-        glVertex2i(197,313);
-        glVertex2i(188,313);
+        glVertex2f(188,317);
+        glVertex2f(197,317);
+        glVertex2f(197,313);
+        glVertex2f(188,313);
 
-        glVertex2i(190,313);
-        glVertex2i(197,313);
-        glVertex2i(197,308);
-        glVertex2i(190,308);
+        glVertex2f(190,313);
+        glVertex2f(197,313);
+        glVertex2f(197,308);
+        glVertex2f(190,308);
 
-        glVertex2i(191,308);
-        glVertex2i(197,308);
-        glVertex2i(197,304);
-        glVertex2i(191,304);
+        glVertex2f(191,308);
+        glVertex2f(197,308);
+        glVertex2f(197,304);
+        glVertex2f(191,304);
 
     //BADAN_SHADOWS
     glColor3ub(169,8,0);
     //DEPAN
-        glVertex2i(192,321);
-        glVertex2i(197,321);
-        glVertex2i(197,320);
-        glVertex2i(192,320);
+        glVertex2f(192,321);
+        glVertex2f(197,321);
+        glVertex2f(197,320);
+        glVertex2f(192,320);
 
-        glVertex2i(196,320);
-        glVertex2i(198,320);
-        glVertex2i(197,319);
-        glVertex2i(196,319);
+        glVertex2f(196,320);
+        glVertex2f(198,320);
+        glVertex2f(197,319);
+        glVertex2f(196,319);
 
-        glVertex2i(197,319);
-        glVertex2i(198,320);
-        glVertex2i(198,306);
-        glVertex2i(197,306);
+        glVertex2f(197,319);
+        glVertex2f(198,320);
+        glVertex2f(198,306);
+        glVertex2f(197,306);
     //BELAKANG
-        glVertex2i(171,338);
-        glVertex2i(172,338);
-        glVertex2i(172,312);
-        glVertex2i(171,312);
+        glVertex2f(171,338);
+        glVertex2f(172,338);
+        glVertex2f(172,312);
+        glVertex2f(171,312);
 
-        glVertex2i(172,334);
-        glVertex2i(174,334);
-        glVertex2i(174,311);
-        glVertex2i(172,311);
+        glVertex2f(172,334);
+        glVertex2f(174,334);
+        glVertex2f(174,311);
+        glVertex2f(172,311);
 
-        glVertex2i(174,324);
-        glVertex2i(174,311);
-        glVertex2i(176,311);
-        glVertex2i(176,324);
+        glVertex2f(174,324);
+        glVertex2f(174,311);
+        glVertex2f(176,311);
+        glVertex2f(176,324);
 
-        glVertex2i(176,319);
-        glVertex2i(186,319);
-        glVertex2i(186,311);
-        glVertex2i(176,311);
+        glVertex2f(176,319);
+        glVertex2f(186,319);
+        glVertex2f(186,311);
+        glVertex2f(176,311);
 
-        glVertex2i(186,317);
-        glVertex2i(188,317);
-        glVertex2i(188,311);
-        glVertex2i(186,311);
+        glVertex2f(186,317);
+        glVertex2f(188,317);
+        glVertex2f(188,311);
+        glVertex2f(186,311);
 
-        glVertex2i(173,311);
-        glVertex2i(173,310);
-        glVertex2i(187,310);
-        glVertex2i(187,311);
+        glVertex2f(173,311);
+        glVertex2f(173,310);
+        glVertex2f(187,310);
+        glVertex2f(187,311);
 
-        glVertex2i(188,313);
-        glVertex2i(190,313);
-        glVertex2i(190,304);
-        glVertex2i(188,304);
+        glVertex2f(188,313);
+        glVertex2f(190,313);
+        glVertex2f(190,304);
+        glVertex2f(188,304);
 
-        glVertex2i(190,304);
-        glVertex2i(190,308);
-        glVertex2i(191,308);
-        glVertex2i(191,304);
+        glVertex2f(190,304);
+        glVertex2f(190,308);
+        glVertex2f(191,308);
+        glVertex2f(191,304);
 
-        glVertex2i(189,304);
-        glVertex2i(196,304);
-        glVertex2i(196,303);
-        glVertex2i(189,303);
+        glVertex2f(189,304);
+        glVertex2f(196,304);
+        glVertex2f(196,303);
+        glVertex2f(189,303);
 
     //PUNGGUNG
     glColor3ub(202,11,1);
 
-        glVertex2i(164,334);
-        glVertex2i(171,334);
-        glVertex2i(171,315);
-        glVertex2i(164,315);
+        glVertex2f(164,334);
+        glVertex2f(171,334);
+        glVertex2f(171,315);
+        glVertex2f(164,315);
 
-        glVertex2i(164,333);
-        glVertex2i(164,316);
-        glVertex2i(163,316);
-        glVertex2i(163,333);
+        glVertex2f(164,333);
+        glVertex2f(164,316);
+        glVertex2f(163,316);
+        glVertex2f(163,333);
 
     glColor3ub(169,8,0);
 
-        glVertex2i(166,316);
-        glVertex2i(163,316);
-        glVertex2i(163,327);
-        glVertex2i(166,327);
+        glVertex2f(166,316);
+        glVertex2f(163,316);
+        glVertex2f(163,327);
+        glVertex2f(166,327);
 
-        glVertex2i(166,329);
-        glVertex2i(171,329);
-        glVertex2i(171,315);
-        glVertex2i(166,315);
+        glVertex2f(166,329);
+        glVertex2f(171,329);
+        glVertex2f(171,315);
+        glVertex2f(166,315);
 
-        glVertex2i(164,316);
-        glVertex2i(164,315);
-        glVertex2i(171,315);
-        glVertex2i(171,316);
+        glVertex2f(164,316);
+        glVertex2f(164,315);
+        glVertex2f(171,315);
+        glVertex2f(171,316);
 
     //CHARA HELM
     glColor3ub(114,158,168);
 
-        glVertex2i(179,335);
-        glVertex2i(179,326);
-        glVertex2i(193,326);
-        glVertex2i(193,335);
+        glVertex2f(179,335);
+        glVertex2f(179,326);
+        glVertex2f(193,326);
+        glVertex2f(193,335);
 
-        glVertex2i(178,334);
-        glVertex2i(178,327);
-        glVertex2i(194,327);
-        glVertex2i(194,334);
+        glVertex2f(178,334);
+        glVertex2f(178,327);
+        glVertex2f(194,327);
+        glVertex2f(194,334);
 
     glColor3ub(146,202,205);
 
-        glVertex2i(180,335);
-        glVertex2i(180,328);
-        glVertex2i(193,328);
-        glVertex2i(193,335);
+        glVertex2f(180,335);
+        glVertex2f(180,328);
+        glVertex2f(193,328);
+        glVertex2f(193,335);
 
     glEnd();
     glFlush();
